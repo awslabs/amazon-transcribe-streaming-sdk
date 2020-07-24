4a352e2fe9927c14c431df4c62ca8d78112d1bf5
Jordan Guymon <jogu.sd@gmail.com>
2020-07-24 14:00:04 -0700

    Add a realistic end to end test

---
+++ tests/integration/__init__.py
@@ -0,0 +1,5 @@
+import os.path
+
+INTEG_DIR = os.path.dirname(os.path.abspath(__file__))
+ASSETS_DIR = os.path.join(INTEG_DIR, 'assets')
+TEST_WAV_PATH = os.path.join(ASSETS_DIR, 'test.wav')
--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -1,8 +1,11 @@
 from io import BytesIO
+import asyncio
 import pytest
 
+from transcribe.model import TranscriptEvent
 from transcribe.client import TranscribeStreamingClient
 from transcribe.exceptions import BadRequestException
+from tests.integration import TEST_WAV_PATH
 
 
 class TestClientStreaming:
@@ -10,17 +13,41 @@ class TestClientStreaming:
     def client(self):
         return TranscribeStreamingClient("us-west-2")
 
+
+    @pytest.fixture
+    def wav_bytes(self):
+        with open(TEST_WAV_PATH, 'rb') as f:
+            raw_bytes = f.read()
+        # This simulates reading bytes from some asynchronous source
+        # This could be coming from an async file, microphone, etc
+        async def byte_generator():
+            chunk_size = 1024 * 4
+            for i in range(0, len(raw_bytes), chunk_size):
+                yield raw_bytes[i:i+chunk_size]
+                await asyncio.sleep(0.1)
+        return byte_generator
+
+
     @pytest.mark.asyncio
-    async def test_client_start_transcribe_stream(self, client):
+    async def test_client_start_transcribe_stream(self, client, wav_bytes):
         stream = await client.start_stream_transcription(
             language_code="en-US", media_sample_rate_hz=16000, media_encoding="pcm",
         )
 
-        await stream.input_stream.send_audio_event(audio_chunk=b"test")
+        async for chunk in wav_bytes():
+            await stream.input_stream.send_audio_event(audio_chunk=chunk)
         await stream.input_stream.end_stream()
 
+        last_transcript = ''
         async for event in stream.output_stream:
-            pass
+            if not isinstance(event, TranscriptEvent):
+                continue
+            results = event.transcript.results
+            for result in results:
+                for alt in result.alternatives:
+                    last_transcript = alt.transcript
+        # Assert that we got some words back as the service may change its response
+        assert len(last_transcript.split(' ')) != 0
 
     @pytest.mark.asyncio
     async def test_client_start_transcribe_stream_bad_request(self, client):
