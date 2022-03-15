9ae0fb281d9788e9d153b35f620bc88b4f0feafa
Jordan Guymon <jogu.sd@gmail.com>
2022-03-15 15:54:15 -0700

    v0.5.2

--- amazon_transcribe/__init__.py
+++ amazon_transcribe/__init__.py
@@ -12,7 +12,7 @@
 # language governing permissions and limitations under the License.
 
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 from awscrt.io import ClientBootstrap, DefaultHostResolver, EventLoopGroup
 
