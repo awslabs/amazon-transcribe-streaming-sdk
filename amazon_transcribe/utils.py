91e2e91691f23d148d7c99bf6f8511ea2ccee9d1
Jonathan Gaytan <gytndd@88665a372bde.ant.amazon.com>
2022-07-20 10:24:06 -0700

    Improved naming of the time elapsed variable

--- amazon_transcribe/utils.py
+++ amazon_transcribe/utils.py
@@ -53,10 +53,10 @@ async def apply_realtime_delay(
 ) -> None:
     """Applies a delay when reading an audio file steam to simulate a real-time delay."""
     start_time = time.time()
-    total_audio_sent = 0.0
+    elapsed_audio_time = 0.0
     async for chunk in reader:
         await stream.input_stream.send_audio_event(audio_chunk=chunk)
-        total_audio_sent += len(chunk) / (bytes_per_sample * sample_rate * channel_nums)
+        elapsed_audio_time += len(chunk) / (bytes_per_sample * sample_rate * channel_nums)
         # sleep to simulate real-time streaming
-        wait_time = start_time + total_audio_sent - time.time()
+        wait_time = start_time + elapsed_audio_time - time.time()
         await asyncio.sleep(wait_time)
