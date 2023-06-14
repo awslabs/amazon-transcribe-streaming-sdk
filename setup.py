b0be3c31d4be8c07b6b868c9c4db074d3edb98fb
David Miller <45697098+dlm6693@users.noreply.github.com>
2023-06-14 14:30:09 -0400

    Update awscrt to ~=0.16.0 (#93)

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-requires = ["awscrt~=0.14.0"]
+requires = ["awscrt~=0.16.0"]
 
 setup(
     name="amazon-transcribe",
