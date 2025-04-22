b40a10abd2bdfa9effea48a5b80efaf54068e7ce
Miroslav Batchkarov <mirobat@amazon.com>
2025-04-22 13:07:05 +0200

    update deprecated CI action that breaks the build; black

--- amazon_transcribe/endpoints.py
+++ amazon_transcribe/endpoints.py
@@ -34,7 +34,7 @@ class StaticEndpointResolver(BaseEndpointResolver):
 class _TranscribeRegionEndpointResolver(BaseEndpointResolver):
     async def resolve(self, region: str) -> str:
         """Apply region to transcribe uri template."""
-        if region.startswith('cn'):
+        if region.startswith("cn"):
             return f"https://transcribestreaming.{region}.amazonaws.com.cn"
         else:
             return f"https://transcribestreaming.{region}.amazonaws.com"
